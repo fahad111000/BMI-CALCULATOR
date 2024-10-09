e1375303aa06ca995cca4a77d00fe82bb2959a11
fahad111000 <fahad.kh2210@gmail.com>
2024-10-10 01:11:18 +0500

    Added conditional statments

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
+var inputWeight = document.getElementById('weight');
 var inputHeight = document.getElementById('height');
-var inputHeight = document.getElementById('weight');
 
-
-
+// Button Event
 function clicked() {
-    console.log("Button CLicked..");
+    var BMI = inputWeight.value / (inputHeight.value * inputHeight.value);
+    console.log(BMI.toFixed(3))
+    if (BMI < 18.5) {
+        console.log("Underweight")
+    }
+
+    else if (BMI >= 18.5 && BMI <= 24.9) {
+        console.log("Healthy Weight");
+    }
+
+    else if (BMI >= 25.0 && BMI <= 29.9) {
+        console.log("Over Weight");
+    }
+
+    else {
+        console.log("Obesity");
+    }
+
 }
