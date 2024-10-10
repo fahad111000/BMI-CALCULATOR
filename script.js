643099b4cd4c639f76d21f9acc83f7f7f1175d68
fahad111000 <fahad.kh2210@gmail.com>
2024-10-11 01:59:27 +0500

    getting and setting  values

--- script.js
+++ script.js
@@ -1,34 +1,49 @@
 
-// Weight value
-var inputWeight = document.getElementById('weight');
+// Table Values
+var tableHeight = document.getElementById('tableHeight');
+var tableWeight = document.getElementById('tableWeight');
+var tableBmi = document.getElementById('tableBmi');
+var tableStatus = document.getElementById('tableStatus');
+var tableBlock = document.getElementById('tableBlock');
 
-// Height value(Meters)
-var inputHeight = document.getElementById('height');
 
 // Button Event
 function clicked() {
-    var BMI = inputWeight.value / (inputHeight.value * inputHeight.value);
+    // Inputs
+    var inputWeight = document.getElementById('weight').value;
+    var inputHeight = document.getElementById('height').value;
 
-    if(inputHeight.value == '' && inputWeight.value == '') {
-        inputHeight.style.border = '1px solid red';
-        inputWeight.style.border = '1px solid red';
-    }
+    // BMI Value
+    var BMI = inputWeight / (inputHeight * inputHeight);
+    BMI = BMI.toFixed(2);
 
-    console.log(BMI.toFixed(3))
     if (BMI < 18.5) {
-        console.log("Underweight")
+        tableStatus.innerHTML = ` <strong> Underweight </strong>`;
     }
 
     else if (BMI >= 18.5 && BMI <= 24.9) {
-        console.log("Healthy Weight");
+        tableStatus.innerHTML = ` <strong> Healthy </strong>`;
+
     }
 
     else if (BMI >= 25.0 && BMI <= 29.9) {
-        console.log("Over Weight");
+        tableStatus.innerHTML = ` <strong> Overweight </strong>`;
     }
 
     else {
-        console.log("Obesity");
+        tableStatus.innerHTML = ` <strong> Obesity </strong>`;
     }
 
+    tableBlock.style.display = 'block';
+    tableHeight.innerText = `${inputHeight}`;
+    tableWeight.innerText = `${inputWeight}`;
+    tableBmi.innerText = `${BMI}`;
+
+
+}
+
+function restart() {
+    tableBlock.style.display = 'none';
+    inputHeight = '';
+    inputWeight = '';
 }
